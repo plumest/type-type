5a9e094ba134cf25f3256fc9911abec3126ec207
plumest <plumest.chayathon@gmail.com>
2022-06-13 17:48:20 +0700

    Styled Word component

---
+++ src/lib/Word.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let word: string;
+  export let typedWord: string;
+
+  let renderWord: string = word + typedWord.slice(word.length);
+</script>
+
+<p class="word">
+  {#each renderWord as text, index}
+    {#if typedWord.length <= index}
+      <letter class="">{text}</letter>
+    {:else}
+      <letter class={word[index] === typedWord[index] ? "active" : "incorrect"}
+        >{text}</letter
+      >
+    {/if}
+  {/each}
+</p>
+
+<style lang="scss">
+  @import "../styles/colors.scss";
+
+  .word {
+    display: inline-block;
+
+    letter {
+      color: $sub-color;
+      font-size: 24px;
+    }
+
+    .active {
+      color: $text-color;
+    }
+
+    .incorrect {
+      color: $error-color;
+    }
+  }
+</style>
---
+++ src/pages/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Layout from "../lib/__layout.svelte";
+
+  import Word from "../lib/Word.svelte";
+</script>
+
+<Layout>
+  <div class="word-container">
+    <div class="words">
+      <Word word="text" typedWord="texted" />
+      <Word word="text" typedWord="te" />
+      <Word word="text" typedWord="tesdf" />
+
+      <Word word="ดูหมิ่น" typedWord="ดูหมิ่น" />
+      <Word word="ดูหมิ่น" typedWord="ดูหมิ" />
+      <Word word="ดูหมิ่น" typedWord="ดุหมิ้อห" />
+    </div>
+  </div>
+</Layout>
+
+<style lang="scss">
+  .word-container {
+    display: grid;
+    margin: auto;
+    text-align: center;
+  }
+</style>
